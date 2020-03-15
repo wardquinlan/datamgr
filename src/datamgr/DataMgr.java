2097fa61f2866fba839594d07ed30880105cad0b
Ward Quinlan <wardquinlan@outlook.com>
2020-03-15 11:44:35 -0400

    data mgr

--- src/datamgr/DataMgr.java
+++ src/datamgr/DataMgr.java
@@ -1,6 +1,12 @@
 package datamgr;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 public class DataMgr {
+	private static Log log = LogFactory.getFactory().getInstance(DataMgr.class);
+	
     public static void main(String[] args) {
+    	log.info("Hello world");
     }
 }
